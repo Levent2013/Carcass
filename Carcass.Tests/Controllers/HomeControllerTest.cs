a826924ef4c95818b9880c0df933c550e1bf170a
Artem Kustikov <artem.kustikov@gmail.com>
2012-10-03 13:06:28 +0300

    Controllers UT fix

--- Carcass.Tests/Controllers/HomeControllerTest.cs
+++ Carcass.Tests/Controllers/HomeControllerTest.cs
@@ -17,24 +17,14 @@ namespace Carcass.Tests.Controllers
         {
             // Arrange
             HomeController controller = new HomeController();
-
-            // Act
             ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
         }
 
         [TestMethod]
         public void About()
         {
-            // Arrange
             HomeController controller = new HomeController();
-
-            // Act
             ViewResult result = controller.About() as ViewResult;
-
-            // Assert
             Assert.IsNotNull(result);
         }
     }
