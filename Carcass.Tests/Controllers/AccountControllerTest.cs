c9ab224ce646c3ae06a5164281cad81f934fbe24
Artem Kustikov <artem.kustikov@gmail.com>
2012-10-15 01:50:56 +0300

    UT fixed

--- Carcass.Tests/Controllers/AccountControllerTest.cs
+++ Carcass.Tests/Controllers/AccountControllerTest.cs
@@ -2,14 +2,19 @@
 using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Autofac;
 using Autofac.Integration.Mvc;
+using AutoMapper;
+using AutoMapper.Configuration;
 
 using Carcass.Common.Data;
+using Carcass.Common.Data.Extensions;
+using Carcass.Models;
 using Carcass.Controllers;
 using Carcass.Data.Entities;
 using Subtext.TestLibrary;
@@ -44,11 +49,20 @@ namespace Carcass.Tests.Controllers
                 _users.Add(user);
             }
 
+            Mapper.CreateMap<UserEntity, User>()
+                .ForMember(p => p.Id, opt => opt.MapFrom(src => src.UserEntityId))
+                .IgnoreAllNonExisting();
+
+
             var builder = new ContainerBuilder();
             
             builder.RegisterType<QueryBuilder>().As<IQueryBuilder>();
-            var usersRepo = new Repository<UserEntity>(_users);
-            builder.RegisterInstance(usersRepo).As<IRepository<UserEntity>>().SingleInstance();
+            var entitiesRepo = new Repository<UserEntity>(_users);
+            var usersRepo = new Repository<User>(_users.Select(p 
+                => p.MapTo<User>()));
+
+            builder.RegisterInstance(entitiesRepo).As<IRepository<UserEntity>>().SingleInstance();
+            builder.RegisterInstance(usersRepo).As<IRepository<User>>().SingleInstance();
             
             var container = builder.Build();
             var resolver = new AutofacDependencyResolver(container);
@@ -65,12 +79,12 @@ namespace Carcass.Tests.Controllers
                 AccountController controller = new AccountController(DependencyResolver.Current.GetService<IQueryBuilder>());
                 ViewResult result = controller.Users() as ViewResult;
 
-                Assert.IsInstanceOfType(result.Model, typeof(IEnumerable<UserEntity>));
-                var users = result.Model as IEnumerable<UserEntity>;
+                Assert.IsInstanceOfType(result.Model, typeof(IEnumerable<User>));
+                var users = result.Model as IEnumerable<User>;
 
                 Assert.AreEqual(users.Count(), _users.Count());
-                Assert.AreEqual(users.First().UserEntityId, 1);
-                Assert.AreEqual(users.Last().UserEntityId, 10);
+                Assert.AreEqual(users.First().Id, 1);
+                Assert.AreEqual(users.Last().Id, 10);
             }
         }
     }
